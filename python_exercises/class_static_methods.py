e3aee87afd27f7265eb5514da5345bbdce7a7f3a
ajay1706 <ajay97456@gmail.com>
2019-08-22 12:42:28 +0530

    static methods

--- python_exercises/class_static_methods.py
+++ python_exercises/class_static_methods.py
@@ -4,3 +4,11 @@ class Foo:
 		print(cls.__name__)
 my_object = Foo()
 my_object.hi()
+
+class Bar:
+	@staticmethod
+	def hi():
+		print("I don't take param")
+anoth_object = Bar()
+anoth_object.hi()
+		
